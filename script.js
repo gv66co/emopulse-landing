fa5207966a5ee5aae9280348186dc8411e5e3442
gv66co <gerovejo66@gmail.com>
2025-12-27 16:58:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const API_URL = "https://emopulse-api-1009590211108.europe-west4.run.app";
+async function analyzeFrame(frame) {
+  try {
+    const response = await fetch(`${API_URL}/analyze`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ frame })
+    });
 
+    const data = await response.json();
+    console.log("API response:", data);
+    return data;
+
+  } catch (error) {
+    console.error("API error:", error);
+  }
+}
